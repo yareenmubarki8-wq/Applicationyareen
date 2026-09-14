455cd5933447301dd3afd0b90d4d3223e91003e4
class01 <sorakaashwah14@gmail.com>
2026-09-14 10:45:06 +0300

    r5herhejejetfzdhzdjtz

---
+++ app/src/main/java/y/mubarki/applicationyareen/data/MyUserTable/MyUser.java
@@ -0,0 +1,32 @@
+package y.mubarki.applicationyareen.data.MyUserTable;
+
+import androidx.room.ColumnInfo;
+
+public class MyUser {
+    public long keyid;
+    @ColumnInfo(name = "full_Name")//اعطاء اسم جديد للعامود-الصفة في الجدول
+    public String fullName;
+    public String email;//بحالة لم يتم اعطاء اسم للعامود يكون اسم الصفه هو اسم العامود
+    public String phone;
+    public String passw;
+
+    @Override
+    public String toString() {
+        return "MyUser{" +
+                "keyid=" + keyid +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", passw='" + passw + '\'' +
+                '}';
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
+
